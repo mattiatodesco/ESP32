1572ab8b3f3ffe1cb324f50e372537319818c696
mattiatodesco <mattia.todesco@marconirovereto.it>
2026-01-15 21:35:25 +0100

    Read_Voltage: aggiungi commenti

--- 081108_Read_Voltage/081108_Read_Voltage.ino
+++ 081108_Read_Voltage/081108_Read_Voltage.ino
@@ -1,23 +1,36 @@
+/* 
+  Lettura del valore di un potenziometro e visualizzazione della tensione corrispondente.
+  Controllo della luminosità di un LED tramite PWM in base alla lettura del potenziometro.
+*/
+
+// Definizione di costanti
 #define LED 5
 #define POTENZIOMETRO 34
 #define FREQ 1000
 #define RES 8
 
 void setup() {
+  // Inizializzazione della comunicazione seriale
   Serial.begin(115200);
+  // Configurazione del pin del LED come uscita PWM
   ledcAttach(LED, FREQ, RES);
   
 }
 
 void loop() {
+  // Lettura e visualizzazione del valore dal potenziometro
   double read = analogRead(POTENZIOMETRO);
-   Serial.print(read);
-   Serial.print("  ");
-   double percent = read/4095;
-   Serial.println(percent * 3.3);
+  Serial.print(read);
+  Serial.print("  ");
 
-   ledcWrite(LED, percent * (2^RES));
+  // Calcolo della percentuale e visualizzazione della tensione corrispondente
+  double percent = read/4095;
+  Serial.println(percent * 3.3);
 
-   delay(50);
+  // Impostazione del valore PWM sul LED in base alla lettura del potenziometro
+  ledcWrite(LED, percent * (2^RES));
+
+  // Breve pausa prima della prossima lettura
+  delay(50);
    
 }
