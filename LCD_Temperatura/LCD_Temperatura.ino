3eb2e0a3138c74b61067b8a068d99df09081065d
Mattia Todesco <mattia.todesco@marconirovereto.it>
2026-02-19 09:31:36 +0100

    (LCD_Temperatura): aggiungi visualizzazione orario su schermo, da sistemare il reset con tasto

--- LCD_Temperatura/LCD_Temperatura.ino
+++ LCD_Temperatura/LCD_Temperatura.ino
@@ -8,14 +8,22 @@ LiquidCrystal lcd(19, 23, 18, 17, 16, 15);
 #define VREF_mV    1266.0 // fondoscala ADC in millivolt
 #define res 4095.0  // risoluzione predefinita 12 bit
 #define sensore 34 // ESP32 pin GPIO36 (ADC0) connesso a LM35
+#define pulsante 22
+
+unsigned long ultimoReset = 0;
  
 void setup() {
+  Serial.begin(9600);
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
   lcd.print("Temperatura");
 
   analogSetAttenuation(ADC_0db);
+
+  pinMode(pulsante, INPUT_PULLDOWN);
+
+  ultimoReset = (int)millis();
 }
  
 void loop() {
@@ -33,6 +41,34 @@ void loop() {
   // print the number of seconds since reset:
   
   lcd.print(tempC);
+
+  boolean letturaBottone = digitalRead(pulsante); 
+  if (letturaBottone){
+    ultimoReset = (int)millis();
+  }
+
+  unsigned long millisecondiAttuali = millis() - ultimoReset;
+  unsigned long totaleSecondi = millisecondiAttuali / 1000;
+
+  int secondi = totaleSecondi % 60;
+  int minuti = (totaleSecondi / 60) % 60;
+  int ore = (totaleSecondi / 3600);
+  
+  lcd.setCursor(8, 1);
+  if(ore < 10) lcd.print("0"); // Formattazione 00:00:00
+  lcd.print(ore);
+  lcd.print(":");
+  if(minuti < 10) lcd.print("0");
+  lcd.print(minuti);
+  lcd.print(":");
+  if(secondi < 10) lcd.print("0");
+  lcd.print(secondi);  
+
+  Serial.print(ore);
+  Serial.print(":");
+  Serial.print(minuti);
+  Serial.print(":");
+  Serial.println(secondi);
 
   delay(500);
 }
