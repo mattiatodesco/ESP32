b43297e90b724e8833e66c333568d9e31987c3bd
mattiatodesco <mattia.todesco@marconirovereto.it>
2026-03-19 03:45:18 +0100

    feat(wifi_02): aggiungi supporto per ping verso un altro ESP32 e aggiorna commenti

--- wifi/Wifi_02/Wifi_02.ino
+++ wifi/Wifi_02/Wifi_02.ino
@@ -1,17 +1,23 @@
-/* Wifi_02.cpp - Connessione alla rete specificata nel file ConfigWifi.h e visualizzazione dell'IPv4 assegnato dal DHCP*/
+/* Wifi_02.cpp - Connessione alla rete specificata nel file ConfigWifi.h,
+   visualizzazione dell'IPv4 assegnato dal DHCP e ping verso un altro ESP32 */
 #include <WiFi.h>
+#include <ESP32Ping.h>
 #include "ConfigWifi.h"
 
 const int LED_ON_BOARD = 2;
+
+// IP dell'altro ESP32 collegato alla stessa rete
+IPAddress ipRemoto(192, 168, 1, 50);
 
 // dichiarazione funzioni di servizio
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_ON_BOARD, OUTPUT);
+
   // Imposta l'ESP32 in modalità Station
-  WiFi.mode(WIFI_STA); 
-  
+  WiFi.mode(WIFI_STA);
+
   Serial.println("\nConnessione in corso...");
   WiFi.begin(ssid, password);
 
@@ -23,14 +29,30 @@ void setup() {
 
   Serial.println("\nConnesso correttamente!");
   Serial.print("Indirizzo IP locale: ");
-  Serial.println(WiFi.localIP());           // Stampa l'IP assegnato dal DHCP
+  Serial.println(WiFi.localIP());   // Stampa l'IP assegnato dal DHCP
+
+  Serial.print("Ping verso il dispositivo con IP: ");
+  Serial.println(ipRemoto);
 }
 
 void loop() {
   digitalWrite(LED_ON_BOARD, HIGH);
+
+  Serial.println("\nInvio ping...");
+  bool esitoPing = Ping.ping(ipRemoto, 3);
+
+  if (esitoPing) {
+    Serial.println("Host raggiungibile.");
+    Serial.print("Tempo medio di risposta: ");
+    Serial.print(Ping.averageTime());
+    Serial.println(" ms");
+  } else {
+    Serial.println("Host non raggiungibile.");
+  }
+
   delay(1000);
   digitalWrite(LED_ON_BOARD, LOW);
-  delay(1000);
+  delay(4000);
 }
 
 // definizione funzioni di servizio
